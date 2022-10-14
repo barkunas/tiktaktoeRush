cbfdd96baa5b8efd0c9bf205579ce0f140d43d36
alexeyb <alexeyb@playtika.com>
2022-10-15 01:21:57 +0300

    test mouse events

--- src/components/Game/Platforms/Platforms.tsx
+++ src/components/Game/Platforms/Platforms.tsx
@@ -1,7 +1,8 @@
 import { OBJModel } from "../../Helpers/OBJModel";
 import { MutableRefObject, Ref, useEffect, useRef, useState } from "react";
-import { AxesHelper, Box3, BufferGeometry, Euler, Material, Mesh, Vector3 } from "three";
+import { AxesHelper, Box3, BufferGeometry, Color, Euler, Material, Mesh, MeshPhysicalMaterial, Vector3 } from "three";
 import { ResourcesPaths } from "../../../configs/resources"
+import { ThreeEvent } from "@react-three/fiber";
 
 
 export function Platforms() {
@@ -19,27 +20,56 @@ export function Platforms() {
 
     })*/
     const axesHelper = new AxesHelper(5);
+    const platformOnPointerEnterHandler = (e: ThreeEvent<MouseEvent>) => {
+        const target = e.eventObject as Mesh;
+        const material = target.material as MeshPhysicalMaterial
+        material.color = new Color(0xffCfff)
+    }
+    const platformOnPointerLeaveHandler = (e: ThreeEvent<MouseEvent>) => {
+        const target = e.eventObject as Mesh;
+        const material = target.material as MeshPhysicalMaterial
+        material.color = new Color(0xffffff)
+    }
     return (
         <>
             <axesHelper/>
             <group position={[-8, 0, 0]}>
-                <OBJModel ref={ref} path={ResourcesPaths.platform1} position={[0, 0, 0]}/>
+                <OBJModel ref={ref} path={ResourcesPaths.platform1} position={[0, 0, 0]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
                 <group position={[0, 0, 4]}>
-                    <OBJModel path={ResourcesPaths.platform1} />
-                    <OBJModel path={ResourcesPaths.point1} position={[2, 0, 1]} rotation={new Euler(0,0.8)}/>
+                    <OBJModel path={ResourcesPaths.platform1}
+                              onPointerEnter={platformOnPointerEnterHandler}
+                              onPointerLeave={platformOnPointerLeaveHandler}
+                    />
+                    <OBJModel path={ResourcesPaths.point1} position={[2, 0, 1]} rotation={new Euler(0, 0.8)}/>
                 </group>
-                <OBJModel path={ResourcesPaths.platform1} position={[0, 0, 8]}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[0, 0, 8]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
 
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 0]}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 4]}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 8]}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 0]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 4]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 8]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
 
                 <group position={[8, 0, 0]}>
-                    <OBJModel path={ResourcesPaths.platform1} />
+                    <OBJModel path={ResourcesPaths.platform1}
+                              onPointerEnter={platformOnPointerEnterHandler}
+                              onPointerLeave={platformOnPointerLeaveHandler}/>
                     <OBJModel path={ResourcesPaths.point2}/>
                 </group>
-                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 4]}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 8]}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 4]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
+                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 8]}
+                          onPointerEnter={platformOnPointerEnterHandler}
+                          onPointerLeave={platformOnPointerLeaveHandler}/>
             </group>
         </>
     );
