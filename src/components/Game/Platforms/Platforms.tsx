c8e053d3a176a2bb89a2588ab7a295d1085b8a4f
alexeyb <alexeyb@playtika.com>
2022-10-15 02:15:32 +0300

    add

--- src/components/Game/Platforms/Platforms.tsx
+++ src/components/Game/Platforms/Platforms.tsx
@@ -1,12 +1,13 @@
 import { OBJModel } from "../../Helpers/OBJModel";
-import {  useRef } from "react";
-import { Color, Euler, Mesh, MeshPhysicalMaterial } from "three";
+import { useState } from "react";
+import { Color, Euler, Mesh, MeshPhysicalMaterial, Object3D } from "three";
 import { ResourcesPaths } from "../../../configs/resources"
 import { ThreeEvent } from "@react-three/fiber";
 
-
 export function Platforms() {
-    const ref = useRef<Mesh>(null!);
+    const [model, setModel] = useState([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    const [activeUser, setActiveUser] = useState(true)
+
     console.log("Mesh")
     /*useEffect(() => {
         const mesh = ref.current;
@@ -29,46 +30,49 @@ export function Platforms() {
         const material = target.material as MeshPhysicalMaterial
         material.color = new Color(0xffffff)
     }
+    const platformOnClickHandler = (e: ThreeEvent<MouseEvent>) => {
+        const target = e.eventObject as Object3D;
+        const position = target.name.split("-");
+        const x = Number(position[0]);
+        const y = Number(position[1]);
+        console.log(e)
+        model[x][y]=+activeUser+1
+        setModel([...model])
+        setActiveUser(!activeUser)
+    }
+    const platforms: JSX.Element[] = []
+    const offset = [0, 4, 8];
+    model.forEach((row, rowInd) => {
+        row.forEach((cell, cellInd) => {
+            const x = offset[rowInd];
+            const y = offset[cellInd];
+            let item: JSX.Element | null = null;
+            switch (model[rowInd][cellInd]) {
+                case 1:
+                    item = <OBJModel path={ResourcesPaths.point1} position={[2, 0, 1]} rotation={new Euler(0, 0.8)}/>;
+                    break;
+                case 2:
+                    item = <OBJModel path={ResourcesPaths.point2}/>;
+                    break;
+            }
+            platforms.push(
+                <group position={[y, 0, x]} key={`${rowInd}-${cellInd}`}>
+                    <OBJModel path={ResourcesPaths.platform1}
+                              name={`${rowInd}-${cellInd}`}
+                              onPointerEnter={platformOnPointerEnterHandler}
+                              onPointerLeave={platformOnPointerLeaveHandler}
+                              onClick={platformOnClickHandler}
+                    />
+                    {item}
+                </group>
+            )
+        })
+    })
     return (
         <>
             <axesHelper/>
             <group position={[-8, 0, 0]}>
-                <OBJModel ref={ref} path={ResourcesPaths.platform1} position={[0, 0, 0]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-                <group position={[0, 0, 4]}>
-                    <OBJModel path={ResourcesPaths.platform1}
-                              onPointerEnter={platformOnPointerEnterHandler}
-                              onPointerLeave={platformOnPointerLeaveHandler}
-                    />
-                    <OBJModel path={ResourcesPaths.point1} position={[2, 0, 1]} rotation={new Euler(0, 0.8)}/>
-                </group>
-                <OBJModel path={ResourcesPaths.platform1} position={[0, 0, 8]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 0]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 4]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[4, 0, 8]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-
-                <group position={[8, 0, 0]}>
-                    <OBJModel path={ResourcesPaths.platform1}
-                              onPointerEnter={platformOnPointerEnterHandler}
-                              onPointerLeave={platformOnPointerLeaveHandler}/>
-                    <OBJModel path={ResourcesPaths.point2}/>
-                </group>
-                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 4]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
-                <OBJModel path={ResourcesPaths.platform1} position={[8, 0, 8]}
-                          onPointerEnter={platformOnPointerEnterHandler}
-                          onPointerLeave={platformOnPointerLeaveHandler}/>
+                {platforms}
             </group>
         </>
     );
