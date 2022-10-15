ba189f5d0814d3e4bdb7927bf1fe23477b4e95df
alexeyb <alexeyb@playtika.com>
2022-10-15 14:42:54 +0300

    cleanup

--- src/components/Game/Platforms/Platforms.tsx
+++ src/components/Game/Platforms/Platforms.tsx
@@ -2,19 +2,27 @@ import { useState } from "react";
 import { ItemType, PlatformItem } from "./PlatformItem";
 
 export type PlatformsModel = ItemType[][];
-export type UpdateModelFn = (model:PlatformsModel)=>void
+export type UpdateModelFn = (model: PlatformsModel) => void
 
-const initialModel:PlatformsModel = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+const initialModel: PlatformsModel = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 export function Platforms() {
     const [model, setModel] = useState(initialModel)
-    const updateModel:UpdateModelFn = (model:PlatformsModel)=>{
+    const updateModel: UpdateModelFn = (model: PlatformsModel) => {
         setModel([...model])
     }
     const platforms: JSX.Element[] = []
     model.forEach((row, rowInd) => {
         row.forEach((cell, cellInd) => {
-            platforms.push(<PlatformItem rowIndex={rowInd} cellIndex={cellInd} itemType={model[rowInd][cellInd]} model={model} updateModel={updateModel}/>);
+            platforms.push(
+                <PlatformItem
+                    rowIndex={rowInd}
+                    cellIndex={cellInd}
+                    itemType={model[rowInd][cellInd]}
+                    model={model}
+                    updateModel={updateModel}
+                />
+            );
         })
     })
     return (
